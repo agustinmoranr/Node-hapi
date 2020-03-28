8dc6d8728f87e977dda6850d07d7a7dd3406d6eb
Agustin Moran <agustin.moranr@gmail.com>
2020-03-27 22:35:55 -0600

    clase 5: se descargo la carpeta public de los archivos de la clase, que contiene el index.html y css de nuestra aplicación, se installo el plugin inert, para poder servir staticos y poder implementar nuestro front-end

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi');
+const inert = require('@hapi/inert'); // plugin para servir archivos estáticos
+const path = require('path'); // modulo nativo de node para gestionar rutas
 
 const server = Hapi.server({
     port: process.env.PORT || 3000,
     host: 'localhost',
+    routes: {   //objeto de rutas
+        files: { // propiedad files
+            relativeTo: path.join(__dirname, 'public') 
+        } // relativeTo define desde donde la ruta va a tomar los archivos (public)
+    }
 })
     
     async function init () {
-        server.route({ //definimos las características de la ruta y req
-            method: 'GET',
-            path: '/',
-            handler: (req, h) => { //h es un objeto que nos ayuda a modificar la respuesta al cliente
-               return h.response("Hola Mundo!").code(200);
-            }
-        })
-        server.route({
-            method: 'GET',
-            path: '/redirect',
-            handler: (req, h) => {
-               return h.redirect("http://platzi.com");
-            }
-        })
         try{
+            await server.register(inert) //server.register registra el plugin 
+
+            server.route({ //definimos las características de la ruta y req
+                method: 'GET',
+                path: '/home',
+                handler: (req, h) => { //h es un objeto que nos ayuda a modificar la respuesta al cliente
+                   return h.file('index.html').code(200) //h.file sirve un archivo 
+                }
+            })
+            server.route({
+                method: 'GET',
+                path: '/{param*}',
+                handler: {
+                    directory: {
+                        path: '.', // desde donde se serviran los archivos (osea el directorio actual que es public)
+                        index: ['index.html']  
+                    } 
+                }
+            })
+
             await server.start();
         }
         catch(error) {
             console.error(error);
             process.exit(1);
         }
-        console.log(`Servidor ejecutandose en el puerto: ${server.info.uri}`)
+        console.log(`Servidor ejecutándose en el puerto: ${server.info.uri}`)
     }
     
 init();
