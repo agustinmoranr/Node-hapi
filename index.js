875bc23f81a43ba0fbaa89bbe7a0e95188d19135
Agustin Moran <agustin.moranr@gmail.com>
2020-03-28 00:14:50 -0600

    clase 7: Renderizando las vistas: en esta clase implementamos vision al proyecto para gestionar el rederizado de nuestras vistas con ayuda de handlerbars, creamos la carpeta views y definimos server.views en el index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi');
+const handlebars = require('handlebars');
 const inert = require('@hapi/inert'); // plugin para servir archivos estáticos
 const path = require('path'); // modulo nativo de node para gestionar rutas
+const vision = require('@hapi/vision'); // plugin para hacer render de motores de plantillas 
 
 const server = Hapi.server({
     port: process.env.PORT || 3000,
@@ -17,12 +19,26 @@ const server = Hapi.server({
     async function init () {
         try{
             await server.register(inert) //server.register registra el plugin 
+            await server.register(vision)
+
+            server.views ({ // método views para gestionar como utilizaremos nuestras vistas
+                engines: { //hapi puede utilizar distintos motores de plantillas
+                    hbs: handlebars // asociamos el plugin al tipo de archivos .hbs
+                },
+                relativeTo: __dirname, // para que las vistas las busque fuera de /public
+                path: 'views', // directorio donde colocaremos las vistas 
+                layout: true, // indicamos que utilizaremos layouts
+                layoutPath: 'views' // ubicación de los layout  
+            })
 
             server.route({ //definimos las características de la ruta y req
                 method: 'GET',
-                path: '/home',
+                path: '/',
                 handler: (req, h) => { //h es un objeto que nos ayuda a modificar la respuesta al cliente
-                   return h.file('index.html').code(200) //h.file sirve un archivo 
+                   return h.view('index', { // h.view sirve una vista 
+                       title: "Home", // valor de nuestra variable de handlebars
+                   }) 
+
                 }
             })
             server.route({
